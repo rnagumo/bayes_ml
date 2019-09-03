6bc51fb6a414ad0f9ece5a9017d5c26366e88fab
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2019-09-03 17:49:45 +0900

    [update

---
+++ julia/PoissonMixture.jl
@@ -0,0 +1,230 @@
+
+module PoissonMixture
+
+"""
+Poisson Mixture Model
+
+p(x, z, lmd, pi) = p(x|z, lmd)p(z|pi)p(lmd)p(pi)
+
+p(pi) = Dir(pi|alpha)
+p(lmd) = Gam(lmd_d,k|a, b)
+p(z|pi) = Cat(z_n|pi)
+p(x|z, lmd) = Poi(x_n,d|lmd_d,k)^(z_n,k)
+"""
+
+using LinearAlgebra
+using Distributions
+using StatsFuns
+using SpecialFunctions
+
+export PoissonMixtureModel, sample_model, sample_data, VI, GS
+
+struct PoissonMixtureModel
+    D::Int
+    K::Int
+    alpha::Array{Float64, 1}  # K
+    a::Array{Float64, 2}  # D*K
+    b::Array{Float64, 1}  # K
+end
+
+struct SampledPoissonMixtureModel
+    D::Int
+    K::Int
+    phi::Array{Float64, 1}  # K
+    lambda::Array{Float64, 2}  # D*K
+end
+
+# -----------------------------------------------------------
+# Utils
+# -----------------------------------------------------------
+
+eye(D::Int) = Matrix{Float64}(I, D, D)
+
+function sqsum(mat::Array{Float64}, idx::Int)
+    return dropdims(sum(mat, dims=idx), dims=idx)
+end
+
+function categorical_sample(p::Array{Float64}, N::Int64=1)
+    """
+    Convert sampled index to one-hot vector
+    """
+    K = length(p)
+    S = zeros(N, K)
+    for n in 1:N
+        S[n, rand(Categorical(p))] = 1
+    end
+
+    if N == 1
+        S = S[1, :]
+    end
+
+    return S
+end
+
+# -----------------------------------------------------------
+# Sampling Functions
+# -----------------------------------------------------------
+
+function sample_model(model::PoissonMixtureModel)
+    """
+    Sample paramters
+    """
+    # Dimension
+    D = model.D
+    K = model.K
+
+    # Sample phi
+    phi = rand(Dirichlet(model.alpha))
+
+    # Sample lambda
+    lambda = zeros(D, K)
+    for d in 1:D
+        for k in 1:K
+            lambda[d, k] = rand(Gamma(model.a[d, k], 1.0 / model.b[k]))
+        end
+    end
+
+    return SampledPoissonMixtureModel(D, K, phi, lambda)
+end
+
+function sample_data(N::Int, model::SampledPoissonMixtureModel)
+    """
+    Sample data
+    """
+    # Dimension
+    D = model.D
+    K = model.K
+
+    # Sample S (latent variable)
+    S = zeros(N, K)
+    for n in 1:N
+        S[n, :] = categorical_sample(model.phi)
+    end
+
+    # Sample X (data)
+    X = zeros(N, D)
+    for n in 1:N
+        for d in 1:D
+            X[n, d] = rand(Poisson(model.lambda[d, argmax(S[n, :])]))
+        end
+    end
+
+    return X, S
+end
+
+# -----------------------------------------------------------
+# Variational Inference Functions
+# -----------------------------------------------------------
+
+function init_latent_variable(X::Array{Float64}, prior::PoissonMixtureModel)
+
+    # Dimension
+    N = size(X, 1)
+    K = prior.K
+    S = categorical_sample(ones(K) ./ K, N)
+    return S
+end
+
+function update_S(X::Array{Float64}, posterior::PoissonMixtureModel)
+
+    # Dimension
+    N = size(X, 1)
+    K = posterior.K
+
+    # Expectations
+    lmd_expt = posterior.a ./ posterior.b'
+    ln_lmd_expt = digamma.(posterior.a) .- log.(posterior.b)'
+    ln_phi_expt = digamma.(posterior.alpha) .- digamma(sum(posterior.alpha))
+
+    # Update S
+    ln_S_expt = zeros(N, K)
+    for n in 1:N
+        for k in 1:K
+            ln_S_expt[n, k] = (X[n, :]' * ln_lmd_expt[:, k]
+                                  - sum(lmd_expt[:, k])
+                                  + ln_phi_expt[k])
+        end
+        ln_S_expt[n, :] .-= logsumexp(ln_S_expt[n, :])
+    end
+
+    return exp.(ln_S_expt)
+end
+
+function update_posterior(S::Array{Float64, 2}, X::Array{Float64},
+                           prior::PoissonMixtureModel)
+
+    alpha = sqsum(S, 1) + prior.alpha
+    a = X' * S + prior.a
+    b = sqsum(S, 1) + prior.b
+
+    return PoissonMixtureModel(prior.D, prior.K, alpha, a, b)
+end
+
+function VI(X::Array{Float64}, prior::PoissonMixtureModel, max_iter::Int)
+    """
+    Variational Inference for PoissonMixtureModel
+    """
+    # Initialization
+    S_expt = init_latent_variable(X, prior)
+    posterior = update_posterior(S_expt, X, prior)
+
+    # Inference
+    for iter in 1:max_iter
+        # E-step
+        S_expt = update_S(X, posterior)
+
+        # M-step
+        posterior = update_posterior(S_expt, X, prior)
+    end
+
+    return posterior, S_expt
+end
+
+# -----------------------------------------------------------
+# Gibbs Sampling Functions
+# -----------------------------------------------------------
+
+function sample_S(X::Array{Float64}, posterior::PoissonMixtureModel)
+
+    # Dimension
+    N = size(X, 1)
+    K = posterior.K
+
+    # Sample parameters
+    model = sample_model(posterior)
+
+    # Sample S
+    ln_S = zeros(N, K)
+    for n in 1:N
+        for k in 1:K
+            ln_S[n, k] = (X[n, :]' * log.(model.lambda[:, k])
+                              - sum(model.lambda[:, k])
+                              + log.(model.phi[k]))
+        end
+        ln_S[n, :] .-= logsumexp(ln_S[n, :])
+    end
+
+    return exp.(ln_S)
+end
+
+function GS(X::Array{Float64}, prior::PoissonMixtureModel, max_iter::Int)
+    """
+    Gibbs sampling for PoissonMixtureModel
+    """
+
+    S = init_latent_variable(X, prior)
+    posterior = update_posterior(S, X, prior)
+
+    # Inference
+    for iter in 1:max_iter
+        # Sample latent variable
+        S = sample_S(X, posterior)
+
+        # update posterior
+        posterior = update_posterior(S, X, prior)
+    end
+
+    return posterior, S
+end
+
+end
---
+++ julia/example_PoissonMixture.jl
@@ -0,0 +1,197 @@
+
+"""
+Example for NMF (Non-negative Matrix Factorization)
+"""
+
+using LinearAlgebra
+using PyPlot, PyCall
+using ArgParse
+using Distributions
+
+push!(LOAD_PATH, ".")
+import PoissonMixture
+
+# -----------------------------------------------------------
+# Executing function
+# -----------------------------------------------------------
+
+function check_with_dummy_data()
+
+    # Hyper-parameters
+    D = 2
+    K = 3
+    alpha = 100.0 * ones(K)
+    a = ones(D, K)
+    b = 0.01 * ones(K)
+
+    # Prior
+    prior = PoissonMixture.PoissonMixtureModel(D, K, alpha, a, b)
+
+    # Sample dummy data
+    N = 10
+    sampled_model = PoissonMixture.sample_model(prior)
+    X, S = PoissonMixture.sample_data(N, sampled_model)
+
+    # Inference
+    max_iter = 1
+    posterior, S_est = PoissonMixture.VI(X, prior, max_iter)
+    # posterior, S_est = PoissonMixture.GS(X, prior, max_iter)
+    
+    println(S_est)
+end
+
+function test_2d_plot()
+
+    # ---------------------------------------------------------
+    # Sample data
+    # ---------------------------------------------------------
+
+    D = 2
+    K = 6
+    alpha = 100.0 * ones(K)
+    a = ones(D, K)
+    b = 0.01 * ones(K)
+    prior = PoissonMixture.PoissonMixtureModel(D, K, alpha, a, b)
+
+    # Sample dummy data
+    N = 300
+    sampled_model = PoissonMixture.sample_model(prior)
+    X, S = PoissonMixture.sample_data(N, sampled_model)
+
+    # ---------------------------------------------------------
+    # Model
+    # ---------------------------------------------------------
+
+    # Inference & Visualization
+    max_iter = 100
+    posterior, S_est = PoissonMixture.VI(X, prior, max_iter)
+    # posterior, S_est = PoissonMixture.GS(X, prior, max_iter)
+
+    # Visualization
+    S_cat = get_argmax(S)
+    S_est_cat = get_argmax(S_est)
+    colors = ["b", "r", "g", "orange", "c", "grey"]
+
+    figure(figsize=(12, 6))
+    subplot(121)
+    scatter(X[:, 1], X[:, 2], color=colors[S_cat])
+    title("Truth")
+
+    subplot(122)
+    scatter(X[:, 1], X[:, 2], color=colors[S_est_cat])
+    title("Estimation")
+
+    tight_layout()
+    try
+        show()
+    catch
+        close()
+    end
+end
+
+function test_2d_plot_with_save()
+    # Prior
+    D = 2
+    K = 6
+    alpha = 100.0 * ones(K)
+    a = ones(D, K)
+    b = 0.01 * ones(K)    
+    prior = PoissonMixture.PoissonMixtureModel(D, K, alpha, a, b)
+
+    # Sample dummy data
+    N = 500
+    sampled_model = PoissonMixture.sample_model(prior)
+    X, S = PoissonMixture.sample_data(N, sampled_model)
+
+    # Inference & Visualization
+    max_iter = 100
+
+    # -----------------------------------------------------
+    # Variational Inference
+    # -----------------------------------------------------
+
+    posterior, S_est = PoissonMixture.VI(X, prior, max_iter)
+
+    # Visualization
+    S_cat = get_argmax(S)
+    S_est_cat = get_argmax(S_est)
+    colors = ["b", "r", "g", "orange", "c", "grey", "k"]
+
+    figure(figsize=(12, 6))
+    subplot(121)
+    scatter(X[:, 1], X[:, 2], color=colors[S_cat])
+    title("Truth")
+
+    subplot(122)
+    scatter(X[:, 1], X[:, 2], color=colors[S_est_cat])
+    title("Estimation")
+
+    suptitle("Variational Inference")
+    tight_layout()
+    savefig("../results/pmm_vi.png")
+    close()
+
+    # -----------------------------------------------------
+    # Variational Inference
+    # -----------------------------------------------------
+    posterior, S_est = PoissonMixture.GS(X, prior, max_iter)
+
+    # Visualization
+    S_cat = get_argmax(S)
+    S_est_cat = get_argmax(S_est)
+
+    figure(figsize=(12, 6))
+    subplot(121)
+    scatter(X[:, 1], X[:, 2], color=colors[S_cat])
+    title("Truth")
+
+    subplot(122)
+    scatter(X[:, 1], X[:, 2], color=colors[S_est_cat])
+    title("Estimation")
+
+    suptitle("Gibbs sampling")
+    tight_layout()
+    savefig("../results/pmm_gs.png")
+    close()
+end
+
+function get_argmax(X::Array)
+    N = size(X, 1)
+    idx = zeros(Int32, N)
+    for n in 1:N
+        idx[n] = argmax(X[n, :])
+    end
+
+    return idx
+end
+
+# -----------------------------------------------------------
+# Main function
+# -----------------------------------------------------------
+
+function parse_commandline()
+    s = ArgParseSettings()
+
+    @add_arg_table s begin
+        "func"
+            help = "selected function"
+            arg_type = String
+            default = "dummy"
+    end
+
+    return parse_args(s)
+end
+
+function main()
+    parsed_args = parse_commandline()
+
+    # Function hash table
+    func_dict = Dict([("dummy", check_with_dummy_data),
+                      ("plot", test_2d_plot),
+                      ("save", test_2d_plot_with_save)])
+
+    # Execute selected function
+    func_dict[parsed_args["func"]]()
+end
+
+main()
