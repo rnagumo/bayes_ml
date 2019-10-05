a74d4555dd2a17245876cca4122b8b677e0a18ef
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2019-10-05 15:28:36 +0900

    [add] hmc

---
+++ julia/MCMC.jl
@@ -0,0 +1,159 @@
+
+module MCMC
+
+"""
+MCMC with 2d Gaussian
+"""
+
+using LinearAlgebra
+using Distributions
+using StatsFuns
+using SpecialFunctions
+
+eye(D::Int) = Matrix{Float64}(I, D, D)
+
+function metropolis_hastings(mu::Array, Sigma::Array, max_iter::Int)
+    """
+    Metroplis Hastings with Gaussian kernel
+
+    In this case, this is also called random-walk Metropolis
+    """
+
+    # Dimension
+    D = size(Sigma, 1)
+
+    # Sample
+    sample_list = zeros(max_iter, D)
+    z0 = randn(D)
+
+    # Acceptance count
+    acpt_cnt = 0
+
+    for iter in 1:max_iter
+        # Sample new state from proposal
+        # Nomral Gauss (random-walk), symmetric q(z1|z0) = q(z0|z1)
+        # In this case, MH equals to Metroplis algorithm
+        z1 = rand(MvNormal(z0, eye(D)))
+
+        # Calculate probability of target distribution
+        # with Gaussian kernel
+        z0_dist = exp(-0.5 * (z0 - mu)' * Sigma * (z0 - mu))
+        z1_dist = exp(-0.5 * (z1 - mu)' * Sigma * (z1 - mu))
+        
+        if rand() <= min(1, z1_dist / z0_dist)
+            # Accept
+            acpt_cnt += 1
+            sample_list[iter, :] = z1
+            z0 = z1
+        else
+            # Reject
+            sample_list[iter, :] = z0
+        end
+    end
+
+    return sample_list, round(acpt_cnt / max_iter, digits=2)
+end
+
+function hamiltonian_montecarlo(mu::Array, Sigma::Array, max_iter::Int,
+                                epsilon::Float64=1.0, L::Int=2, burnin::Int=5)
+    """
+    Hamiltonian Monte Carlo
+
+    ref) MCMC using Hamiltonian dynamics
+    http://arxiv.org/abs/1206.1901
+    """
+
+    # Dimension
+    D = size(Sigma, 1)
+
+    # Potential energy
+    U(q::Array) = q' * q / 2
+    grad_U(q::Array) = q
+
+    # Kinetic energy
+    K(p::Array) = p' * p / 2
+    grad_K(p::Array) = p
+    
+    # Sample
+    sample_list = zeros(max_iter + burnin, D)
+    current_q = randn(D)
+    
+    # Acceptance count
+    acpt_cnt = 0
+
+    for iter in 1:(max_iter + burnin)
+        # Initialization
+        q = deepcopy(current_q)
+        current_p = randn(D)
+        p = deepcopy(current_p)
+
+        # HMC Step
+        # 1. Half step for momentum
+        p = p - epsilon .* grad_U(q) ./ 2
+
+        # 2. L step for momentum and position
+        for i in 1:L
+            # Full step for position
+            q = q + epsilon .* grad_K(p)
+
+            # Full step for momentum, except last step
+            if i != L
+                p = p - epsilon .* grad_U(q)
+            end
+        end
+
+        # 3. Final half step for momentum
+        p = p - epsilon .* grad_U(q) ./ 2
+
+        # Negate momentum at end of trajectory
+        p = -p
+
+        # Evaluate potential
+        current_H = U(current_q) + K(current_p)
+        proposed_H = U(q) + K(p)
+
+        if rand() <= min(1, exp(-proposed_H + current_H))
+            # Accept
+            sample_list[iter, :] = q
+            acpt_cnt += 1
+            current_q = q
+        else
+            # Reject
+            sample_list[iter, :] = current_q
+        end
+    end
+
+    sample_list = sample_list[burnin:max_iter + burnin, :]
+
+    return sample_list, round(acpt_cnt / (max_iter + burnin), digits=2)
+end
+
+function gibbs_sampling(mu::Array, Sigma::Array, max_iter::Int)
+    # Dimension
+    D = size(mu, 1)
+
+    # Sample
+    sample_list = zeros(max_iter * 2, D)
+    z0 = randn(D)
+
+    current = deepcopy(z0)
+    for iter in 1:max_iter
+        # Sample z1 from conditional Gaussian
+        mu_12 = mu[1] + Sigma[1, 2] * inv(Sigma[2, 2]) * (current[2] - mu[2])
+        sigma_12 = Sigma[1, 1] - Sigma[1, 2] * inv(Sigma[2, 2]) * Sigma[2, 1]
+        z1 = rand(Normal(mu_12, sigma_12))
+        current[1] = z1
+        sample_list[iter * 2 - 1, :] = current
+
+        # Sample z2 from conditional Gaussian
+        mu_21 = mu[2] + Sigma[2, 1] * inv(Sigma[1, 1]) * (current[1] - mu[1])
+        sigma_21 = Sigma[2, 2] - Sigma[2, 1] * inv(Sigma[1, 1]) * Sigma[1, 2]
+        z2 = rand(Normal(mu_21, sigma_21))
+        current[2] = z2
+        sample_list[iter * 2, :] = current
+    end
+
+    return sample_list
+end
+
+end
---
+++ julia/example_MCMC.jl
@@ -0,0 +1,151 @@
+
+"""
+Example for MCMC
+"""
+
+using LinearAlgebra
+using PyPlot, PyCall
+using ArgParse
+using Distributions
+
+push!(LOAD_PATH, ".")
+import MCMC
+
+# -----------------------------------------------------------
+# Executing function
+# -----------------------------------------------------------
+
+function check_with_dummy_data()
+    # Target Gaussian distribution
+    mu = [0.0, 0.0]
+    Sigma = [1 0.9; 0.9 1]
+    max_iter = 5
+
+    # Sample
+    samples_mh, acpt_rate_mh = MCMC.metropolis_hastings(mu, Sigma, max_iter)
+    samples_hmc, acpt_rate_hmc = MCMC.hamiltonian_montecarlo(
+        mu, Sigma, max_iter)
+    samples_gibbs = MCMC.gibbs_sampling(mu, Sigma, max_iter)
+
+    println(size(samples_mh), acpt_rate_mh)
+    println(size(samples_hmc), acpt_rate_hmc)
+    println(size(samples_gibbs))
+end
+
+function test_2d_plot()
+    # Target Gaussian distribution
+    mu = [0.0, 0.0]
+    Sigma = [1 0.9; 0.9 1]
+    max_iter = 100
+
+    samples_mh, acpt_rate_mh = MCMC.metropolis_hastings(mu, Sigma, max_iter)
+    samples_hmc, acpt_rate_hmc = MCMC.hamiltonian_montecarlo(
+        mu, Sigma, max_iter, 0.3, 4, 5)
+    samples_gibbs = MCMC.gibbs_sampling(mu, Sigma, max_iter)
+
+    # println(samples_hmc[1:10, :])
+
+    # -------------------------------------------------------------
+    # Visualization
+    # -------------------------------------------------------------
+
+    p_pdf = gauss_pdf_1s(mu, Sigma)
+
+    # Plot
+    figure(figsize=(12, 6))
+
+    subplot(131)
+    plot(p_pdf[:, 1], p_pdf[:, 2], alpha=0.9)
+    plot(samples_mh[:, 1], samples_mh[:, 2], "--o", alpha=0.6,
+         label="acceptance ratio = $acpt_rate_mh")
+    legend(loc="upper right")
+    title("Metropolis Hastings")
+
+    subplot(132)
+    plot(p_pdf[:, 1], p_pdf[:, 2], alpha=0.9)
+    plot(samples_hmc[:, 1], samples_hmc[:, 2], "--o", alpha=0.6,
+         label="acceptance ratio = $acpt_rate_hmc")
+    legend(loc="upper right")
+    title("Hamiltonian Monte Carlo")
+
+    subplot(133)
+    plot(p_pdf[:, 1], p_pdf[:, 2], alpha=0.9)
+    plot(samples_gibbs[:, 1], samples_gibbs[:, 2], "--o", alpha=0.6,
+         label="acceptance ratio = 1.0")
+    legend(loc="upper right")
+    title("Gibbs sampling")
+
+    tight_layout()
+    savefig("../results/mcmc.png")
+
+    try
+        show()
+    catch
+        close()
+    end
+end
+
+function gauss_pdf_1s(mu::Array, Sigma::Array, res::Int=100, c::Float64=1.0)
+    """
+    Return Gaussian pdf (probability density function) in 1 sigma
+
+    Parameters
+    ----------
+    mu : Array
+        Mean of Gaussian
+
+    Sigma : Array
+        Covariance of Gaussian
+
+    res : Int
+        Resolution of curve
+
+    c : Float
+        Scaling coefficient
+
+    Returns
+    -------
+    P : Array, size(res + 1, 2)
+    """
+    # Eigenvalues, eigenvectors
+    val, vec = eigen(Sigma)
+    w = 2 * pi / res .* (0:res)
+
+    a = sqrt(c * val[1])
+    b = sqrt(c * val[2])
+    p1 = a .* cos.(w)
+    p2 = b .* sin.(w)
+    P = repeat(mu', res + 1, 1) + vcat(p1', p2')' * vec
+
+    return P
+end
+
+# -----------------------------------------------------------
+# Main function
+# -----------------------------------------------------------
+
+function parse_commandline()
+    s = ArgParseSettings()
+
+    @add_arg_table s begin
+        "func"
+            help = "selected function"
+            arg_type = String
+            default = "dummy"
+    end
+
+    return parse_args(s)
+end
+
+function main()
+    parsed_args = parse_commandline()
+
+    # Function hash table
+    func_dict = Dict([("dummy", check_with_dummy_data),
+                      ("plot", test_2d_plot)])
+
+    # Execute selected function
+    func_dict[parsed_args["func"]]()
+end
+
+main()
