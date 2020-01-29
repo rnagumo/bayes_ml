23cbb2485d9597000478ca3973666e7fefe26556
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-01-29 21:04:17 +0900

    [add] logger function

---
+++ dl/utils.py
@@ -0,0 +1,50 @@
+
+"""Utils for training"""
+
+import logging
+import pathlib
+import time
+
+
+def init_logger(path):
+    """Initializes logger.
+
+    Set stream and file handler with specified format.
+
+    Parameters
+    ----------
+    path : str
+        Path to logging file directory
+
+    Returns
+    -------
+    logger : logging.Logger
+        Logger
+    """
+
+    log_dir = pathlib.Path(path)
+    if not log_dir.exists():
+        log_dir.mkdir()
+
+    log_fn = "training_{}.log".format(time.strftime("%Y%m%d"))
+    log_path = log_dir.joinpath(log_fn)
+
+    # Initialize logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    # Set stream handler (console)
+    sh = logging.StreamHandler()
+    sh_fmt = logging.Formatter(
+        "%(asctime)s - %(module)s.%(funcName)s - %(levelname)s : %(message)s")
+    sh.setFormatter(sh_fmt)
+    logger.addHandler(sh)
+
+    # Set file handler (log file)
+    fh = logging.FileHandler(filename=log_path)
+    fh_fmt = logging.Formatter(
+        "%(asctime)s - %(module)s.%(funcName)s - %(levelname)s : %(message)s")
+    fh.setFormatter(fh_fmt)
+    logger.addHandler(fh)
+
+    return logger
